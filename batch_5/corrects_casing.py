d9c585fdf2e45182f4a513a5a6508f120ccc9838
Yufillia <yuufiii29@gmail.com>
2025-03-21 16:10:53 +0800

    added program 5

---
+++ batch_5/corrects_casing.py
@@ -0,0 +1,3 @@
+# Ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+fullname = input("Enter your fullname with incorrect casing: ")
+print(fullname.title())
